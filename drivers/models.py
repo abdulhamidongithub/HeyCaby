b28707a626ed3ce3744a122814d7ffc3a38747cb
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-10-12 20:39:20 +0500

    Added a model called Driver

--- drivers/models.py
+++ drivers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class CarType(models.Model):
+class CarCategory(models.Model):
     type = models.CharField(max_length=100)
     minimum = models.PositiveIntegerField()
     waiting_cost = models.PositiveIntegerField()
@@ -9,3 +9,19 @@ class CarType(models.Model):
 
     def __str__(self):
         return self.type
+
+class Driver(models.Model):
+    fullname = models.CharField(max_length=40)
+    phone = models.CharField(max_length=15)
+    car_type = models.CharField(max_length=30)
+    car_number = models.CharField(max_length=10)
+    sms_code = models.CharField(max_length=30)
+    confirmed = models.BooleanField(default=False)
+    gender = models.CharField(max_length=15)
+    balance = models.PositiveIntegerField(default=0)
+    has_baggage = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(CarCategory, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.fullname} - {self.phone}"
