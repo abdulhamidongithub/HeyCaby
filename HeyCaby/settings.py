82114cb66b8cd8a75f62def1f72ea868b43a2547
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-10-12 20:50:54 +0500

    Added django-environ package

--- HeyCaby/settings.py
+++ HeyCaby/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3@hyj4hr7@dd_#aka-_o)r=zs%f#9u@d487q%n@t$6)#aic3w0'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
