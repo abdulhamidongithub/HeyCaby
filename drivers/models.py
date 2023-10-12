5e961bc3f8d53311157b528d17cbfb7a5d95bfa2
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-10-12 17:33:57 +0500

    Added a model called CarType

--- drivers/models.py
+++ drivers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class CarType(models.Model):
+    type = models.CharField(max_length=100)
+    minimum = models.PositiveIntegerField()
+    waiting_cost = models.PositiveIntegerField()
+    bonus_percent = models.PositiveIntegerField()
+    baggage_cost = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.type
