90b68fb3cc95d4174bcb808c09403d51d8129f3f
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-10-18 20:02:26 +0500

    Added Click-uz credentials

--- HeyCaby/settings.py
+++ HeyCaby/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'clickuz',
 
     'operators',
     'drivers',
@@ -119,3 +120,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CLICK_SETTINGS = {
+    "service_id": env("SERVICE_ID"),
+    "merchant_id": env("MERCHANT_ID"),
+    "secret_key": env("CL_SECRET_KEY"),
+    "merchant_user_id": env("MERCH_USER_ID")
+}
+
