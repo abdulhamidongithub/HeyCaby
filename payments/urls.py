2175bfd50b88460a1b84664383c1c7834eb503e1
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-10-26 18:50:03 +0500

    Payme payment system added

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path("", PaymentsAPIView.as_view()),
     path("clickuz/", ClickView.as_view()),
+    path("paymeuz/", PaycomView.as_view()),
 ]
--- payments/views.py
+++ payments/views.py
@@ -7,6 +7,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.db import transaction
+from paycomuz.views import MerchantAPIView
+from paycomuz import Paycom
+from paycomuz.models import Transaction
 
 from .serializers import PaymentSerializer, PaymentReadSerializer
 from .models import *
@@ -102,3 +105,63 @@ class OrderCheckAndPayment(ClickUz):
 
 class ClickView(ClickUzMerchantAPIView):
     VALIDATE_CLASS = OrderCheckAndPayment
+
+# Payme
+
+class CheckOrder(Paycom):
+    def check_order(self, amount, account, *args, **kwargs):
+        driver = Driver.objects.filter(phone=account["order_id"])
+        if not driver.exists():
+            return self.ORDER_NOT_FOND
+        charge = Payment.objects.filter(driver__phone=account["order_id"], completed=False)
+        if charge.exists():
+            charge = charge.last()
+            if float(charge.amount) == float(amount) / 100:
+                return self.ORDER_FOUND
+            else:
+                charge.amount = int((amount) / 100)
+                charge.save()
+                return self.ORDER_FOUND
+        else:
+            Payment.objects.create(
+                driver=Driver.objects.get(phone=account["order_id"]),
+                amount=int(amount),
+                type="Payme",
+                completed=False
+            )
+            return self.ORDER_FOUND
+
+
+    def successfully_payment(self, account, transaction, *args, **kwargs):
+        transaction = Transaction.objects.filter(_id=account["id"])
+        if transaction.exists():
+            charge = Payment.objects.filter(driver__phone=account["order_id"])
+            if charge.exists():
+                charge = charge.last()
+                driver = Driver.objects.get(id=charge.driver.id)
+                driver.balance += charge.amount
+                driver.save()
+                charge.completed = True
+                charge.save()
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def cancel_payment(self, account, transaction, *args, **kwargs):
+        transaction = Transaction.objects.filter(_id=account["id"])
+        if transaction.exists():
+            charge = Payment.objects.filter(driver__phone=account.get('order_id'))
+            if charge.exists():
+                charge = charge.last()
+                charge.delete()
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+class PaycomView(MerchantAPIView):
+    VALIDATE_CLASS = CheckOrder
